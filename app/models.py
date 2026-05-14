79a24e04ea96a10a926e4a4e767a33d66c8689a0
Gabriel <g.pedro0972@gmail.com>
2026-05-14 11:55:18 -0300

    Finalizando tela de detalhes

--- app/models.py
+++ app/models.py
@@ -1,24 +1,34 @@
 from sqlalchemy import String, Integer, Column, Boolean
-from .database import Base, engine, SessionLocal
+from app.database import Base, engine, SessionLocal
 
 class Books(Base):
-    __tablename__= "books"
-    id= Column(Integer, primary_key=True, index=True)
-    title= Column(String, nullable=False)
-    author= Column(String, nullable=False)
-    year= Column(String)
-    description= Column(String)
-    read= Column(Boolean, default=False)
+    __tablename__ = "books"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    author = Column(String, nullable=False)
+    year = Column(String)
+    description = Column(String)
+    page = Column(Integer, default=0)
+    read = Column(Boolean, default=False)
 
 
-# title="Sobre a Tranquilidade da Alma"
-# author= "Sêneca"
-# year="~60 d.C."
-# description= """ Explora como alcançar paz interior em meio às dificuldades da vida. Discute equilíbrio emocional, propósito e a importância da moderação."""
-# read= False
-# novo=Books(title=title,author=author,year=year,
-#              description=description, read=read)
-# db=SessionLocal() 
+# title = "Senhor dos Aneis"
+# author = "Neymar"
+# year = "Quando tava no barcelona"
+# description = (
+#         """ Explora como alcançar paz interior em meio às dificuldades da vida. Discute equilíbrio emocional, propósito e a importância da moderação."""
+#     )
+# read = False
+# page = 12
+# novo = Books(
+#         title=title,
+#         author=author,
+#         year=year,
+#         description=description,
+#         page=page,
+#         read=read,
+#     )
+# db = SessionLocal()
 # db.add(novo)
 # db.commit()
 
